2a9b38bea5e4989a50bf655c7a028982f066165c
Hary Capri <76942747+harycp@users.noreply.github.com>
2024-05-17 21:24:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const ErrorHandler = require("./ErrorHandler");
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
@@ -22,61 +24,119 @@ mongoose
     console.log("Error connecting to database");
   });
 
+// Fungsi untuk membungkus async function dan mencetak errornya
+// Helper Error Async Func
+const wrapAsync = (fn) => {
+  return function (req, res, next) {
+    fn(req, res, next).catch((err) => next(err));
+  };
+};
+
 // Our Code
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
 });
 
-app.get("/books", async (req, res) => {
-  const { genre } = req.query;
-  if (genre) {
-    console.log(genre);
-    const books = await Books.find({ genre });
-    res.render("books/index", { books, genre });
-  } else {
-    const books = await Books.find({});
-    res.render("books/index", { books, genre: "All" });
-  }
-});
+app.get(
+  "/books",
+  wrapAsync(async (req, res) => {
+    const { genre } = req.query;
+    if (genre) {
+      console.log(genre);
+      const books = await Books.find({ genre });
+      res.render("books/index", { books, genre });
+    } else {
+      const books = await Books.find({});
+      res.render("books/index", { books, genre: "All" });
+    }
+  })
+);
 
 app.get("/books/create", (req, res) => {
   res.render("books/create");
 });
 
-app.post("/books", async (req, res) => {
-  const books = new Books(req.body);
-  await books.save();
+app.post(
+  "/books",
+  wrapAsync(async (req, res) => {
+    const books = new Books(req.body);
+    await books.save();
 
-  res.redirect(`/books/${books._id}`);
+    res.redirect(`/books/${books._id}`);
+  })
+);
+
+app.get(
+  "/books/:id/edit",
+  wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const books = await Books.findById(id);
+    res.render("books/edit", { books });
+  })
+);
+
+app.put(
+  "/books/:id",
+  wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const books = await Books.findByIdAndUpdate(id, req.body, {
+      runValidators: true,
+    });
+    res.redirect(`/books/${books._id}`);
+  })
+);
+
+app.delete(
+  "/books/:id",
+  wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    await Books.findByIdAndDelete(id);
+    res.redirect(`/books`);
+  })
+);
+
+app.get(
+  "/books/:id",
+  wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const books = await Books.findById(id);
+
+    res.render("books/show", { books });
+  })
+);
+
+const validatorHandler = (err) => {
+  err.status = 400;
+  err.message = Object.values(err.errors).map((e) => e.message);
+  return new ErrorHandler(err.message, err.status);
+};
+
+const castHandler = (err) => {
+  err.status = 404;
+  err.message = "Book Not Found";
+  return new ErrorHandler(err.message, err.status);
+};
+
+// ada 2 tipe error dalam mongosh
+// cast error dan validation error
+app.use((err, req, res, next) => {
+  console.dir(err);
+  if (err.name === "ValidationError") err = validatorHandler(err);
+  if (err.name === "CastError") {
+    err = castHandler(err);
+  }
+  next(err);
 });
 
-app.get("/books/:id/edit", async (req, res) => {
-  const { id } = req.params;
-  const books = await Books.findById(id);
-  res.render("books/edit", { books });
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something went wrong" } = err;
+  res.status(status).send(message);
 });
 
-app.put("/books/:id", async (req, res) => {
-  const { id } = req.params;
-  const books = await Books.findByIdAndUpdate(id, req.body, {
-    runValidators: true,
-  });
-  res.redirect(`/books/${books._id}`);
-});
-
-app.delete("/books/:id", async (req, res) => {
-  const { id } = req.params;
-  await Books.findByIdAndDelete(id);
-  res.redirect(`/books`);
-});
-
-app.get("/books/:id", async (req, res) => {
-  const { id } = req.params;
-  const books = await Books.findById(id);
-
-  res.render("books/show", { books });
+app.get("*", (req, res) => {
+  res.status(404).send("Page Not Found");
 });
 
 app.listen(8080, () => {
-  console.log("ShopApp server is running on port 8080");
+  console.log("Server is running on port 8080");
 });
